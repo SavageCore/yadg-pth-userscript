52385edd00dff88d02fd124053e8dc1fe60b4fc6
SavageCore <talk@savagecore.eu>
2016-12-04 06:40:50 +0000

    1.3.11

--- pth_yadg.user.js
+++ pth_yadg.user.js
@@ -3,7 +3,7 @@
 // @name           PTH YADG
 // @description    This script provides integration with online description generator YADG (http://yadg.cc) - Credit to Slack06
 // @license        https://github.com/SavageCore/yadg-pth-userscript/blob/master/LICENSE
-// @version        1.3.10
+// @version        1.3.11
 // @namespace      yadg
 // @grant          GM_xmlhttpRequest
 // @require        https://yadg.cc/static/js/jsandbox.min.js
