e77b367370e477436a9db802814334c4e2ee4b90
SavageCore <talk@savagecore.eu>
2017-11-13 19:09:27 +0000

    Support new iTunes urls

--- pth_yadg.user.js
+++ pth_yadg.user.js
@@ -63,9 +63,13 @@ function fetchImage(target, callback) {
 			});
 			break;
 		case (/itunes/.test(link)):
-			var regex = /apple\.com\/([a-z]*)\/.*id(\d+)/;
-			var id = regex.exec(link)[2];
-			var country = regex.exec(link)[1];
+      var regex = /apple\.com\/(?:([a-z]{2,3})\/)?.*\/(?:(\d+)|id(\d*))/;
+      var res = regex.exec(link);
+			var id = res[2] | res[3];
+			var country = 'us';
+			if (res[1]) {
+				country = res[1];
+			}
 			GM_xmlhttpRequest({ // eslint-disable-line new-cap
 				method: 'GET',
 				url: 'https://itunes.apple.com/lookup?id=' + id + '&country=' + country,
